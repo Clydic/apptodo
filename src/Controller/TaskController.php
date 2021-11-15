303188d8fe417a0b90789d6b491eacaa770748ee
Clydic <70671047+Clydic@users.noreply.github.com>
2021-11-15 14:29:30 +0100

    feat : ajout de bootstrap

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Task;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TaskController extends AbstractController
 {
     /**
-     * @Route("/task", name="task")
+     * @Route("/task/listening", name="task")
      */
     public function index(): Response
     {
+        // On va chercher avec la Doctrine le Repository
+        $repository = $this->getDoctrine()->getRepository(Task::class);
+
+        // Dans ce repository nous récupérons toutes les données
+        $tasks = $repository->findAll();
+
+        //Affichage du var_dump
+        //var_dump($tasks);
+        //die;
+        // dd($tasks);
         return $this->render('task/index.html.twig', [
             'controller_name' => 'TaskController',
         ]);
