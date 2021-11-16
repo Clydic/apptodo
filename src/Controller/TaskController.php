e8900a3390211a43669cafabaf25d13114ff1030
Clydic <70671047+Clydic@users.noreply.github.com>
2021-11-16 16:33:33 +0100

    feat ajout des boutons, fonction delete

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -4,16 +4,41 @@ namespace App\Controller;
 
 use App\Entity\Task;
 use App\Form\TaskType;
+use Doctrine\ORM\EntityManager;
 use PhpParser\Node\Expr\FuncCall;
+use App\Repository\TaskRepository;
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 
 class TaskController extends AbstractController
 {
     /**
-     * @Route("/task/listening", name="task")
+     * @var TaskRepository
+     *
+     */
+    private $repository;
+
+    /**
+     * @var EntityManagerInterface
+     *
+     */
+    private $manager;
+
+    public function __construct(
+        TaskRepository $repository,
+        EntityManagerInterface $manager
+    ) {
+        $this->repository = $repository;
+        $this->manager = $manager;
+    }
+
+
+    /**
+     * @Route("/task/listening", name="task-listening")
      */
     public function index(): Response
     {
@@ -34,14 +59,47 @@ class TaskController extends AbstractController
 
     /**
      * @Route("/task/create", name="task_create")
-     *
+     * @Route("/task/update/{id}", name="task_update", requirements={"id"="\d+"})
      */
-    public function createTask(Request $request)
+    public function task(Task $task = null, Request $request)
     {
-        $task = new Task;
+        if (!$task) {
+
+            $task = new Task;
+
+            $task->setCreatedAt(new \DateTime());
+        }
+
 
         $form = $this->createForm(TaskType::class, $task, []);
 
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $task->setName($form['name']->getData())
+                ->setDescription($form['description']->getData())
+                ->setDueAt($form['dueAt']->getData())
+                ->setTag($form['tag']->getData());
+
+            $this->manager->persist($task);
+            $this->manager->flush();
+
+            return $this->redirectToRoute('task-listening');
+        }
+
         return $this->render('task/create.html.twig', ['form' => $form->createView()]);
+    }
+
+    /**
+     * @Route("/task/delete/{id}", name="task_delete", requirements={"id"="\d+"})
+     *
+     */
+    public function deleteTask(Task $task): Response
+    {
+
+        $this->manager->remove($task);
+        $this->manager->flush();
+
+        return $this->redirectToRoute("task-listening");
     }
 }
